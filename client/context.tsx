5e6891a187d8afcddf1619eac5568977b606e875
TSgt Jason Leaver <jason.leaver@us.af.mil>
2022-12-04 19:22:17 -0600

    formatting

--- client/context.tsx
+++ client/context.tsx
@@ -1,38 +1,38 @@
 import React from "react";
 interface FITEProps {
-	apiUrl: string | URL;
-	model: string;
-	strategy: string;
-	textAreaValue?: string;
-	textCompletion?: string;
+  apiUrl: string | URL;
+  model: string;
+  strategy: string;
+  textAreaValue?: string;
+  textCompletion?: string;
 }
 export interface FITEState extends FITEProps {
-	modelOptions?: string[];
-	strategyOptions?: string[];
-	textAreaValue: string;
-	textCompletion: string;
-	textPrompt?: string;
+  modelOptions?: string[];
+  strategyOptions?: string[];
+  textAreaValue: string;
+  textCompletion: string;
+  textPrompt?: string;
 }
 
 const DEFAULT_STATE = {
-	apiUrl: "http://localhost:8000",
-	model: "gpt2-taf-base1",
-	strategy: "GREEDY",
-	textAreaValue: "",
-	textCompletion: "",
+  apiUrl: "http://localhost:8000",
+  model: "gpt2-taf-base1",
+  strategy: "GREEDY",
+  textAreaValue: "",
+  textCompletion: "",
 } as FITEState;
 
 interface ContextState extends FITEState {
-	__setState: (ctx: React.SetStateAction<FITEState>) => void;
+  __setState: (ctx: React.SetStateAction<FITEState>) => void;
 }
 
 export const FITEContext = React.createContext<ContextState>({
-	...DEFAULT_STATE,
-	__setState: () => void 0,
+  ...DEFAULT_STATE,
+  __setState: () => void 0,
 });
 
 export default ({ children, ...initialState }: React.PropsWithChildren<FITEProps>) => {
-	const [state, __setState] = React.useState<FITEState>({ ...DEFAULT_STATE, ...initialState });
+  const [state, __setState] = React.useState<FITEState>({ ...DEFAULT_STATE, ...initialState });
 
-	return <FITEContext.Provider value={{ ...state, __setState }}>{children}</FITEContext.Provider>;
+  return <FITEContext.Provider value={{ ...state, __setState }}>{children}</FITEContext.Provider>;
 };
